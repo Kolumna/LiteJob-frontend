1786ce7b14e61d3102d1190a3e4d2e01ff877992
K0lumna <michaljaniec12@wp.pl>
2022-10-20 11:25:14 +0200

    api rest fetch

--- src/components/TestApi.js
+++ src/components/TestApi.js
@@ -1,36 +1,31 @@
 import { useEffect, useState } from "react";
-import { Button, CircularProgress } from "@mui/material";
-import React from 'react'
+import { CircularProgress } from "@mui/material";
+import React from "react";
 
 const TestApi = () => {
-    const [backendData, setBackendData] = useState([{}])
+  const [backendData, setBackendData] = useState([{}]);
 
-     useEffect(() => {
-        fetch('/api').then(
-            response => response.json()
-        ).then(
-            data => {
-                setBackendData(data)
-            }
-        )
-    }, [])
+  useEffect(() => {
+    fetch("/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
 
-    const [title, newTitle] = useState('Lite')
-    return (
-        <div className="p-8">
+  //const [title, newTitle] = useState("Lite");
+  return (
+    <div className="p-8">
+      <div></div>
+      { typeof backendData === "undefined" ? (
+        <CircularProgress />
+      ) : (
+        backendData.map((element, i) => (
+          <p className=" text-3xl" key={i}>Cześć {element.first_name}!</p>
+        ))
+      )}
+    </div>
+  );
+};
 
-            <div>{title}</div>
-            {(typeof backendData.oferta === 'undefined') ? (
-                <CircularProgress />
-            ) : (
-                backendData.oferta.map((oferta, i) => (
-                    <div key={i}>
-                        <p>{oferta}</p><Button className=" ml-8" variant="contained" onClick={() => newTitle('Job')}>Zobacz</Button>
-                    </div>
-                ))
-            )}
-        </div>
-  )
-}
-
-export default TestApi
+export default TestApi;
