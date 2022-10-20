f9d1196b7907be5ecf248fa91e9090f48a19ff98
K0lumna <michaljaniec12@wp.pl>
2022-10-20 17:14:43 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import NavBar from "./components/NavBar";
 import TestApi from "./components/TestApi";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SignIn from "./components/account/SignIn";
+import SignUp from "./components/SignUp";
 
 const App = () => {
   return (
     <>
       <NavBar />
-      <TestApi />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={TestApi} />
+          <Route exact path="/login" component={SignIn} />
+          <Route exact path="/register" component={SignUp} />
+        </Switch>
+      </Router>
     </>
   );
-}
+};
 
 export default App;
